19b7b6f751457b18d4fa673e2aa440678ac74183
Jigar varma <71844973+Jigarvarma2005@users.noreply.github.com>
2021-10-24 18:20:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,8 @@
+pip3 install --no-cache-dir -r requirements.txt
+cp extract /usr/local/bin
+cp pextract /usr/local/bin
+chmod +x /usr/local/bin/extract && chmod +x /usr/local/bin/pextract
+cp .netrc /root/.netrc
+chmod 600 /usr/src/app/.netrc
+chmod +x aria.sh
 ./aria.sh; python3 -m bot
